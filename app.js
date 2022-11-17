d9d484402e0eca805efba2bebcc423b13b83fc03
Michael <michael@thinkgrow.com>
2022-11-17 10:25:16 -0800

    update

---
+++ app.js
@@ -0,0 +1,28 @@
+const animalArray = ['dog', 'cat', 'pig'];
+
+animalArray.push('cow');
+
+const personObj = {
+  name: 'Lernantino',
+  age: 99
+};
+
+personObj.age = 100;
+personObj.occupation = 'Developer';
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+const printProfileData = profileDataArr => {
+    // This...
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+      console.log(profileDataArr[i]);
+    }
+  
+    console.log('================');
+  
+    // Is the same as this...
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+
+  };
+  printProfileData(profileDataArgs);
