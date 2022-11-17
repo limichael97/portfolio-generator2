a6bc6f7daa354bd29af800849be21ec582919ea3
Michael <michael@thinkgrow.com>
2022-11-17 11:02:35 -0800

    template

--- app.js
+++ app.js
@@ -1,28 +1,39 @@
-const animalArray = ['dog', 'cat', 'pig'];
+// const animalArray = ['dog', 'cat', 'pig'];
 
-animalArray.push('cow');
+// animalArray.push('cow');
 
-const personObj = {
-  name: 'Lernantino',
-  age: 99
-};
+// const personObj = {
+//   name: 'Lernantino',
+//   age: 99
+// };
 
-personObj.age = 100;
-personObj.occupation = 'Developer';
+// personObj.age = 100;
+// personObj.occupation = 'Developer';
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
+// const printProfileData = profileDataArr => {
+//     // This...
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//       console.log(profileDataArr[i]);
+//     }
   
-    console.log('================');
+//     console.log('================');
   
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+//     // Is the same as this...
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
 
-  };
-  printProfileData(profileDataArgs);
+//   };
+//   printProfileData(profileDataArgs);
+
+const fs = require('fs')
+const generatePage = require('./src/page-template.js')
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output')
+})
