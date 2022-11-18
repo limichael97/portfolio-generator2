97a23ef259afa761a2257e8a0e182bd41f3c44aa
Michael <michael@thinkgrow.com>
2022-11-17 19:09:02 -0800

    project section

--- app.js
+++ app.js
@@ -27,17 +27,10 @@
 
 const inquirer = require('inquirer');
 
-// const fs = require('fs')
-// const generatePage = require('./src/page-template.js')
+const fs = require('fs')
+const generatePage = require('./src/page-template.js')
 
-// const pageHTML = generatePage(name, github)
-
-// fs.writeFile('index.html', pageHTML, err => {
-//     if (err) throw err;
-
-//     console.log('Portfolio complete! Check out index.html to see the output')
-// })
-
+// 
 
 const promptUser= () => {
     return inquirer.prompt([
@@ -170,7 +163,14 @@ Add a New Project
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData)
+        const pageHTML = generatePage(portfolioData)
+
+        fs.writeFile('index.html', pageHTML, err => {
+            if (err) throw err;
+
+            console.log('Portfolio complete! Check out index.html to see the output')
+        })
+
     })
 
 
